ab094020c2ec392609190da0d59f1d66dd5be3a1
Brian Jones <drf0124@yahoo.com>
2015-11-19 14:26:12 -0500

    got the edit to work

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,21 +1,28 @@
 class PlayersController < ApplicationController
-before_action :set_team
+
 
 def create 
-	@player = @team.players.create(player_params)
-	redirect_to @team
+	@team = Team.find(params[:team_id])
+	@player = @team.players.create(params[:player].permit(:name, :position, :notes, :why, :why_not))
+	@player.save
+
+	if @player.save
+			redirect_to team_path(@team)
+	else
+			render 'new'
+	end
 end 
 
 def edit 
-		@team = Team.find(params[:id])
-		@player = @team.players(params[:id])
+	@team = Team.find(params[:team_id])
+ 	@player = @team.players.find(params[:id])
 end 
 
 	def update 
 		@team = Team.find(params[:team_id])
-		@player = @team.player.find(params[:id])
+		@player = @team.players.find(params[:id])
 		
-		if player.update(params[:player].permit(:player))
+		if @player.update(params[:player].permit(:name, :position, :notes, :why, :why_not))
 			redirect_to team_path(@team)
 		else 
 			render 'edit'
@@ -23,23 +30,11 @@ end
 	end 
 
 	def destroy 
+		@team = Team.find(params[:team_id])
  		@player = @team.players.find(params[:id])
- 		if @player.destroy
- 			flash[:success] = "Player was deleted."
- 		else 
- 			flash[:error] = "Player could not be deleted."
- 		end 
- 		redirect_to @team
+ 		@player.destroy
+ 		redirect_to team_path(@team)
  	end 
 
-private
-
-	def set_team
-		@team = Team.find(params[:team_id])
-	end
-
-	def player_params
-		params[:player].permit(:name, :position, :note, :why, :why_not)
-	end
 
 end
